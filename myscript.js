c7b1b68d8e9011029cb28866f969aab2c5ae51c1
Takuya <yky2800.rthm.55tn@gmail.com>
2019-01-26 14:35:10 +0900

    alert

--- myscript.js
+++ myscript.js
@@ -1,13 +1,1 @@
-// メッセージを出力
-console.log("メッセージを出力");
-
-// 変数を出力
-var i = 10;
-console.log(i);
-
-// info, warn, error
-console.info("情報です");
-
-console.warn("警告です");
-
-console.error("エラーです");
+alert("アラートです");
