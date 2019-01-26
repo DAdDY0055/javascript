df56c67cc0a78392cb6018bb896778ef0bc67d9c
Takuya <yky2800.rthm.55tn@gmail.com>
2019-01-26 14:34:08 +0900

    info, warn, errorメッセージ出力

--- myscript.js
+++ myscript.js
@@ -1,6 +1,13 @@
-// console.log("Hello World! from myscript.js");
-/*
-console.log("Hello World! from myscript.js");
-console.log("Hello World! from myscript.js");
-console.log("Hello World! from myscript.js");
-*/
+// メッセージを出力
+console.log("メッセージを出力");
+
+// 変数を出力
+var i = 10;
+console.log(i);
+
+// info, warn, error
+console.info("情報です");
+
+console.warn("警告です");
+
+console.error("エラーです");
