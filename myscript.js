f36df3a5b8889ad9d0ceffbd7dfc19fbfef024ed
Takuya <yky2800.rthm.55tn@gmail.com>
2019-01-27 14:35:50 +0900

    1~100のうち偶数の値の合計を出力

--- myscript.js
+++ myscript.js
@@ -1,4 +1,15 @@
-var str = 'こんにちは';
-console.log(str.length);
+for (var i = 1; i <= 3; i++) {
+  console.log(i);
+}
 
-console.log(str.charAt(2));
+var i = 1;
+while (i <= 3) {
+  console.log(i);
+  i++;
+}
+
+var i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 3);
