9feb92bc166e1b686745f4ce725fc80fa646d564
Takuya <yky2800.rthm.55tn@gmail.com>
2019-01-27 20:18:24 +0900

    ES2015

---
+++ allow.js
@@ -0,0 +1,21 @@
+// let f = function(x, y) {
+//   return x * y;
+// };
+
+// let f = (x, y) => {
+//   return x * y;
+// };
+
+// console.log(f(2, 3));
+
+// let f = x => {
+//   console.log(x);
+// };
+
+// f(2);
+
+let f =  => {
+  console.log("test");
+};
+
+f();
---
+++ symbol.js
@@ -0,0 +1,7 @@
+let sym1 = Symbol("foo");
+let sym2 = Symbol("foo");
+
+console.log(typeof sym1);
+console.log(sym1.toString());
+console.log(sym1 === sym1);
+console.log(Symbol("foo") === Symbol("foo"));
