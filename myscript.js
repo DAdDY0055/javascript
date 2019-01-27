8aaaf1d0bcb66820747455395b74cbe67dd610eb
Takuya <yky2800.rthm.55tn@gmail.com>
2019-01-27 13:30:28 +0900

    演習 文字列分割

--- myscript.js
+++ myscript.js
@@ -1,16 +1,4 @@
-var n = 9;
+var str = 'こんにちは';
+console.log(str.length);
 
-switch (n) {
-  case 1:
-    console.log('数値は1です。');
-    break;
-  case 2:
-    console.log('数値は2です。');
-    break;
-  case 3:
-    console.log('数値は3です。');
-    break;
-  default:
-    console.log('範囲外です。');
-    break;
-}
+console.log(str.charAt(2));
---
+++ str_split.js
@@ -0,0 +1,3 @@
+var str = 'red,green,blue';
+
+console.log(str.split(','));
