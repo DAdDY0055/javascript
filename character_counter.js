07c2ad45155bdcde7a47480715d92582e2dc3f1e
Takuya <yky2800.rthm.55tn@gmail.com>
2019-01-27 20:54:14 +0900

    文字数カウンター作成

---
+++ character_counter.js
@@ -0,0 +1,25 @@
+(function() {
+  // 即時関数
+  "use strict"; // strictモード
+
+  var e = document.getElementById("input_text");
+
+  document.getElementById("character_count").innerHTML = "10";
+
+  e.addEventListener("keyup", function() {
+    // console.log(e.value);
+    // console.log(e.value.length);
+    var str = e.value;
+
+    str = str.replace(/\r?\n/g, "");
+    // console.log(str.length);
+    var remaining = 10 - str.length;
+    document.getElementById("character_count").innerHTML = remaining;
+
+    if (remaining >= 0) {
+      document.getElementById("character_count_wrap").style.color = "black";
+    } else {
+      document.getElementById("character_count_wrap").style.color = "red";
+    }
+  });
+})();
